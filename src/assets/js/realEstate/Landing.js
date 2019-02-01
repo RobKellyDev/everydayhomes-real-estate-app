ae6c8b600117eb00224a83f1033a507963897717
Robert Kelly <robertkelly1203@gmail.com>
2019-01-31 22:10:44 -0500

    Added landed page steps section (icons)

--- src/assets/js/realEstate/Landing.js
+++ src/assets/js/realEstate/Landing.js
@@ -10,27 +10,13 @@ class Landing extends Component {
       <div>
         <div id='landing'>
           <div className="landingHeader container">
-            <h1>Search for your perfect home today.</h1>
+            <h1>Find your perfect home today.</h1>
             <Link to="/properties"><button type="button" className="btn btn-danger btn-large landingButtonStyle"><i className="fas fa-home"></i>Residential</button></Link>
             <Link to="/properties"><button type="button" className="btn btn-danger btn-large landingButtonStyle"><i className="fas fa-building"></i>Commercial</button></Link>
           </div>
         </div>
         <div className="container">
           <div className="row">
-            <div className="col-sm-6">
-              <div className="statsBackground">
-                <div className="statsTop">
-                  Average home price sold in past 30 days.
-                </div>
-                <div className="statsMiddle">
-                  <h1 className="statsMiddleBigNumber"><span className="statsMiddleDollar">$</span>689</h1>
-                </div>
-                <div className="statsBottom">
-                  <i className="fas fa-arrow-up statsBottomStyle"><span className="statsBottomNumbers">2.84% 6 Months</span></i>
-                  <i className="fas fa-arrow-down statsBottomStyle"><span className="statsBottomNumbers">2.13% 1 Year</span></i>
-                </div>
-              </div>
-            </div>
             <div className="col-sm-6">
               <div className="statsBackground">
                 <div className="statsTop">
@@ -47,6 +33,34 @@ class Landing extends Component {
                 </div>
               </div>
             </div>
+            <div className="col-sm-6">
+              <div className="statsBackground">
+                <div className="statsTop">
+                  Average home price sold in past 30 days.
+                </div>
+                <div className="statsMiddle">
+                  <h1 className="statsMiddleBigNumber"><span className="statsMiddleDollar">$</span>689</h1>
+                </div>
+                <div className="statsBottom">
+                  <i className="fas fa-arrow-up statsBottomStyle"><span className="statsBottomNumbers">2.84% 6 Months</span></i>
+                  <i className="fas fa-arrow-down statsBottomStyle"><span className="statsBottomNumbers">2.13% 1 Year</span></i>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+              <div className="col-sm-4 steps">
+                <i className="far fa-money-bill-alt"></i>
+                <p>1. Calculate your budget</p>
+              </div>
+              <div className="col-sm-4 steps">
+                <i className="fas fa-home"></i>
+                <p>2. Use our site to find your perfect home</p>
+              </div>
+              <div className="col-sm-4 steps">
+                <i className="fas fa-users"></i>
+                <p>3. Contact one of our helpful agents</p>
+              </div>
           </div>
         </div>
       </div>
