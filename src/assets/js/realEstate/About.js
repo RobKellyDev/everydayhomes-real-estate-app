7c91f173c1a4ddc6cafc64b42613b76a7366efb2
Robert Kelly <robertkelly1203@gmail.com>
2019-02-11 08:00:15 -0500

    Added about page landing image and title

--- src/assets/js/realEstate/About.js
+++ src/assets/js/realEstate/About.js
@@ -5,8 +5,20 @@ export default class About extends Component{
 
   render(){
     return(
-      <div id="about">
-        About
+      <div>
+        <div id="about">
+          <div className="container">
+            <div className="aboutHeaderTitle"><h1>Why choose Everyday Homes?</h1></div>
+            <div className="aboutHeaderText"><p>Everyday Homes is one of the most popular and comprehensive real estate websites for finding homes, our up to date listings and helpful agents can assist you in finding your perfect home for you and your family.</p></div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row allpanels">
+            <div className="col-sm-4"><div className="panel"></div></div>
+            <div className="col-sm-4"><div className="panel"></div></div>
+            <div className="col-sm-4"><div className="panel"></div></div>
+          </div>
+        </div>
       </div>
     )
   }
