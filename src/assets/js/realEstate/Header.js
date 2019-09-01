5c60d207db31c7254a6ae72af43c5be7cbc25bad
Robert Kelly <robertkelly1203@gmail.com>
2019-09-01 11:00:40 -0400

    Fixed navbar menu toggle issue - added Bootstrap JS CDN links

--- src/assets/js/realEstate/Header.js
+++ src/assets/js/realEstate/Header.js
@@ -3,28 +3,26 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from "../../images/logo3.png";
 
+//Header component for app - contains nav bar
 export default class Header extends Component {
-  constructor () {
-    super()
-  }
-
+  //Defines the navabr for the app - uses bootstrap; navbar-expand-lg - indicates the screen size for when icons will be hidden; navbar-toggler - creates the hamburger icon; ml-auto - shifts everything to the left
   render () {
     return (
       <header>
         <div>
           <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
             <Link to="/"><img src={logo} className="logo"/></Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" data-toggle="collapse" data-target="#navcollapseid">
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav navbar ml-auto">
-                <Link to="/properties"><div className="nav-item nav-link active">Buy <span className="sr-only">(current)</span></div></Link>
-                <Link to="/properties"><div className="nav-item nav-link active">Sell <span className="sr-only">(current)</span></div></Link>
-                <Link to="/properties"><div className="nav-item nav-link active">Rent <span className="sr-only">(current)</span></div></Link>
-                <Link to="/about"><div className="nav-item nav-link active">About <span className="sr-only">(current)</span></div></Link>
-                <Link to="/contact"><div className="nav-item nav-link active">Contact <span className="sr-only">(current)</span></div></Link>
-              </div>
+            <div className="collapse navbar-collapse" id="navcollapseid">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item"><Link to="/properties" className="nav-link">Buy</Link></li>
+                <li className="nav-item"><Link to="/properties"className="nav-link">Sell</Link></li>
+                <li className="nav-item"><Link to="/properties"className="nav-link">Rent</Link></li>
+                <li className="nav-item"><Link to="/about"className="nav-link">About</Link></li>
+                <li className="nav-item"><Link to="/contact" className="nav-link">Contact</Link></li>
+              </ul>
             </div>
           </nav>
         </div>
